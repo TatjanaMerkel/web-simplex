1ea06f39503dc0258302d0018d6680dc66353d3a
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-06-16 19:04:41 +0200

    Rename navigation-bar -> nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NavigationBoxComponent } from './navigation-box/navigation-box.component';
 import { LearningComponent } from './pages/learning/learning.component';
 import { PractiseComponent } from './pages/practise/practise.component';
@@ -14,7 +14,7 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent,
+    NavBarComponent,
     NavigationBoxComponent,
     LearningComponent,
     PractiseComponent,
