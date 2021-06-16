077604b5fb1a41d1cff3b06874273da8d0ec0769
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-06-16 20:23:35 +0200

    add example simplex tableau

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -6,6 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  rows = [1, 2];
+  cols = [1, 2, 3];
 
   constructor() {
   }
