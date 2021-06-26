b16ac82340ceee4ed3e743e3838b10b149a7b4c1
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-06-27 00:47:02 +0200

    Show linear system data in standard form

--- src/app/pages/calculator/calculator-content/standard-form/standard-form.component.ts
+++ src/app/pages/calculator/calculator-content/standard-form/standard-form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {TableauData} from "../tableau/tableau-data";
 
 @Component({
   selector: 'app-standard-form',
   templateUrl: './standard-form.component.html',
   styleUrls: ['./standard-form.component.css']
 })
-export class StandardFormComponent implements OnInit {
+export class StandardFormComponent {
 
-  constructor() { }
+  @Input() inputData: TableauData | null = null;
 
-  ngOnInit(): void {
+  @Output() outputDataChange = new EventEmitter<TableauData>();
+
+  outputData: TableauData | null = null;
+
+
+  emitValues() {
+    this.outputDataChange.emit(this.outputData as TableauData)
   }
-
 }
