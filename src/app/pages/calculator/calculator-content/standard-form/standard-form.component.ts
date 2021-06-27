529af4a138551fad0073a41ae2c98c89a947c82e
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-06-27 22:31:51 +0200

    Add slack variables to standard form

--- src/app/pages/calculator/calculator-content/standard-form/standard-form.component.ts
+++ src/app/pages/calculator/calculator-content/standard-form/standard-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {TableauData} from "../tableau/tableau-data";
 
 @Component({
@@ -6,7 +6,7 @@ import {TableauData} from "../tableau/tableau-data";
   templateUrl: './standard-form.component.html',
   styleUrls: ['./standard-form.component.css']
 })
-export class StandardFormComponent {
+export class StandardFormComponent implements OnChanges {
 
   @Input() inputData: TableauData | null = null;
 
@@ -18,4 +18,33 @@ export class StandardFormComponent {
   emitValues() {
     this.outputDataChange.emit(this.outputData as TableauData)
   }
+
+  ngOnChanges(): void {
+    const inputData = this.inputData!;
+
+    const oldTargetVars = inputData.linearSystemData.targetVars;
+    const newTargetVars = new Array<number>(inputData.numberOfConstraints).fill(0);
+
+    this.outputData = {
+      numberOfVars: inputData.numberOfVars + inputData.numberOfConstraints,
+      numberOfConstraints: inputData.numberOfConstraints,
+
+      linearSystemData: {
+        targetVars: oldTargetVars.concat(newTargetVars),
+        constraintVars: new Array<Array<number>>(inputData.numberOfConstraints),
+        constraintConstants: inputData.linearSystemData.constraintConstants
+      }
+    }
+
+    for (let i = 0; i < inputData.linearSystemData.constraintVars.length; i++) {
+      const oldConstraintVarsLine = inputData.linearSystemData.constraintVars[i];
+      const newConstraintVarsLine = new Array<number>(inputData.numberOfConstraints).fill(0);
+
+      newConstraintVarsLine[i] = 1;
+
+      this.outputData.linearSystemData.constraintVars[i] =
+        oldConstraintVarsLine.concat(newConstraintVarsLine);
+    }
+  }
+
 }
