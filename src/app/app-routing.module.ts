4586ddf0e264f82f3824070edb6348989275194a
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-07-28 23:14:49 +0200

    Add /admin/exercises route && Style admin-exercises component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 
-import {HomeComponent} from './home/home.component'
+import {AdminComponent} from './pages/admin/admin.component'
+import {AdminExerciesComponent} from './pages/admin-exercies/admin-exercies.component';
 import {CalculatorComponent} from './pages/calculator/calculator.component'
+import {ExerciseComponent} from './pages/exercise/exercise.component';
+import {HomeComponent} from './home/home.component'
 import {LearningComponent} from './pages/learning/learning.component'
 import {PractiseComponent} from './pages/practise/practise.component'
-import {AdminComponent} from './pages/admin/admin.component'
-import {ExerciseComponent} from './pages/exercise/exercise.component';
+
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'admin', component: AdminComponent},
+  {path: 'admin/exercises', component: AdminExerciesComponent},
   {path: 'calculator', component: CalculatorComponent},
   {path: 'exercises/:exerciseId', component: ExerciseComponent},
   {path: 'learning', component: LearningComponent},
