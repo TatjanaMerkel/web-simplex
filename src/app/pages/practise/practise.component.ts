36b41aff890d036f7bb586cec11aa325b898b4e3
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-07-29 21:02:51 +0200

    Use ExerciseService to get services in practice component

--- src/app/pages/practise/practise.component.ts
+++ src/app/pages/practise/practise.component.ts
@@ -1,18 +1,24 @@
 import {Component, OnInit} from '@angular/core'
 import {ExerciseService} from '../../../services/exercise.service'
+import {Exercise} from '../../../models/exercise'
+
 
 @Component({
   selector: 'app-practise',
   templateUrl: './practise.component.html',
   styleUrls: ['./practise.component.css']
 })
+
 export class PractiseComponent implements OnInit {
 
-  exercises = this.exerciseService.getExercises()
+  exercises: undefined | Exercise[]
 
   constructor(private exerciseService: ExerciseService) {
   }
 
   ngOnInit(): void {
+    this.exerciseService.getExercises().subscribe(exercises => {
+      this.exercises = exercises
+    })
   }
 }
