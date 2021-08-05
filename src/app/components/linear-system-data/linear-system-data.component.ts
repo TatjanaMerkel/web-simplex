aeab1f48dc21b94844f6ef8c50b2fdd6d14cf9f1
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-08-05 22:31:14 +0200

    Support varying linear system size in /admin/exercises/new

--- src/app/components/linear-system-data/linear-system-data.component.ts
+++ src/app/components/linear-system-data/linear-system-data.component.ts
@@ -27,33 +27,85 @@ export class LinearSystemDataComponent implements OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.data) {
-      const current = changes.data.currentValue
       const previous = changes.data.previousValue
+      const current = changes.data.currentValue
 
-      if (!previous
-        || current.numberOfVars !== previous.numberOfVars
-        || current.numberOfConstraints !== previous.numberOfConstraints) {
+      const numberOfVars = this.data.numberOfVars
+      const numberOfConstraints = this.data.numberOfConstraints
 
-        const numberOfVars = this.data.numberOfVars
-        const numberOfConstraints = this.data.numberOfConstraints
+      if (!previous) {
 
-        this.targetVars = new Array<null | Fraction>(numberOfVars).fill(null)
+        this.allocateNewArrays(numberOfVars, numberOfConstraints)
 
-        this.constraintVars = new Array<Array<null | Fraction>>(numberOfConstraints)
-        for (let c = 0; c < this.constraintVars.length; c++) {
-          this.constraintVars[c] = new Array<null | Fraction>(numberOfVars).fill(null)
-        }
+      } else if (this.linearSystemSizeChanged(previous, current)) {
 
-        this.constraintVals = new Array<null | Fraction>(numberOfConstraints).fill(null)
+        const previousTargetVars = this.targetVars!
+        const previousConstraintVars = this.constraintVars!
+        const previousConstraintVals = this.constraintVals!
+
+        this.allocateNewArrays(numberOfVars, numberOfConstraints)
+
+        this.restorePreviousArrays(previousTargetVars, previousConstraintVars, previousConstraintVals)
       }
     }
   }
 
-  //
-  // Event Handlers
-  //
 
-  onTargetVarChanged(event: Event, v: number) {
+  private allocateNewArrays(numberOfVars: number, numberOfConstraints: number): void {
+    this.targetVars = new Array<null | Fraction>(numberOfVars).fill(null)
+
+    this.constraintVars = new Array<Array<null | Fraction>>(numberOfConstraints)
+    for (let c = 0; c < this.constraintVars.length; c++) {
+      this.constraintVars[c] = new Array<null | Fraction>(numberOfVars).fill(null)
+    }
+
+    this.constraintVals = new Array<null | Fraction>(numberOfConstraints).fill(null)
+  }
+
+  private linearSystemSizeChanged(previous: LinearSystemDataInput, current: LinearSystemDataInput): boolean {
+    return current.numberOfVars !== previous.numberOfVars
+      || current.numberOfConstraints !== previous.numberOfConstraints
+  }
+
+  private restorePreviousArrays(
+    previousTargetVars: Array<null | Fraction>,
+    previousConstraintVars: Array<Array<null | Fraction>>,
+    previousConstraintVals: Array<null | Fraction>
+  ): void {
+
+
+    const targetVars = this.targetVars!
+    const constraintVars = this.constraintVars!
+    const constraintVals = this.constraintVals!
+
+
+    for (let v = 0; v < targetVars.length && v < previousTargetVars.length; v++) {
+      targetVars[v] = previousTargetVars[v]
+    }
+
+    for (let c = 0; c < constraintVars.length && c < previousConstraintVars.length; c++) {
+      for (let v = 0; v < constraintVars[c].length && v < previousConstraintVars[c].length; v++) {
+        constraintVars[c][v] = previousConstraintVars[c][v]
+
+      }
+    }
+
+    for (let c = 0; c < constraintVals.length && c < previousConstraintVals.length; c++) {
+      constraintVals[c] = previousConstraintVals[c]
+    }
+
+  }
+
+//
+// Event Handlers
+//
+
+  onTargetVarChanged(event
+                       :
+                       Event, v
+                       :
+                       number
+  ) {
     const targetVars = this.targetVars!
 
     const inputElement = event.target as HTMLInputElement
@@ -69,7 +121,14 @@ export class LinearSystemDataComponent implements OnChanges {
     this.emitData(isValid)
   }
 
-  onConstraintVarChanged(event: Event, c: number, v: number) {
+  onConstraintVarChanged(event
+                           :
+                           Event, c
+                           :
+                           number, v
+                           :
+                           number
+  ) {
     const constraintVars = this.constraintVars!
 
     const inputElement = event.target as HTMLInputElement
@@ -85,7 +144,12 @@ export class LinearSystemDataComponent implements OnChanges {
     this.emitData(isValid)
   }
 
-  onConstraintConstantChanged(event: Event, c: number) {
+  onConstraintConstantChanged(event
+                                :
+                                Event, c
+                                :
+                                number
+  ) {
     const constraintVals = this.constraintVals!
 
     const inputElement = event.target as HTMLInputElement
@@ -101,14 +165,16 @@ export class LinearSystemDataComponent implements OnChanges {
     this.emitData(isValid)
   }
 
-  //
-  // Helper
-  //
+//
+// Helper
+//
 
   /**
    * Check if all input fields are filled.
    */
-  validateInput(): boolean {
+  validateInput()
+    :
+    boolean {
     const targetVars = this.targetVars!
     const constraintVars = this.constraintVars!
     const constraintVals = this.constraintVals!
@@ -126,7 +192,11 @@ export class LinearSystemDataComponent implements OnChanges {
     return constraintVals.indexOf(null) === -1
   }
 
-  emitData(isValid: boolean): void {
+  emitData(isValid
+             :
+             boolean
+  ):
+    void {
     const targetVars = this.targetVars!
     const constraintVars = this.constraintVars!
     const constraintVals = this.constraintVals!
@@ -139,7 +209,12 @@ export class LinearSystemDataComponent implements OnChanges {
     })
   }
 
-  trackByIndex(index: number, _item: any) {
+  trackByIndex(index
+                 :
+                 number, _item
+                 :
+                 any
+  ) {
     return index
   }
 }
