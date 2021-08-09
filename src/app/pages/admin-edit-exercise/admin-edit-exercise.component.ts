2b78f734d905e37ebae1a90fcdf0b1b0b4f1062c
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-08-09 19:16:24 +0200

    Parse mathjs JSON using reviver

--- src/app/pages/admin-edit-exercise/admin-edit-exercise.component.ts
+++ src/app/pages/admin-edit-exercise/admin-edit-exercise.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import {Component, OnInit} from '@angular/core'
+
+import * as math from 'mathjs'
 
 import {Exercise} from '../../../models/exercise'
 import {ExerciseService} from '../../../services/exercise.service'
 import {LinearSystemDataInput} from "../../components/linear-system-data/linear-system-data-input";
-import {ActivatedRoute} from "@angular/router";
-
+import {MathJsJson} from "mathjs";
 
 @Component({
-  selector: 'app-edit-exercise',
+  selector: 'app-admin-edit-exercise',
   templateUrl: './admin-edit-exercise.component.html',
   styleUrls: ['./admin-edit-exercise.component.css']
 })
@@ -19,13 +21,21 @@ export class AdminEditExerciseComponent implements OnInit {
               private exerciseService: ExerciseService) {
   }
 
-
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const exercise_id = Number(params['exercise_id'])
 
-      this.exerciseService.getExercise(exercise_id).subscribe(exercise => {
+      this.exerciseService.getExercise(exercise_id).subscribe((exercise: any) => {
+        const reviver = (math as any).reviver
+
+        const ex: Exercise = {
+          ...exercise,
+          targetVars: JSON.parse(JSON.stringify(exercise.targetVars), reviver),
+          constraintVars: JSON.parse(JSON.stringify(exercise.constraintVars), reviver),
+          constraintVals: JSON.parse(JSON.stringify(exercise.constraintVals), reviver)
+        }
         this.exercise = exercise
+        console.log(exercise)
       })
     })
   }
@@ -39,6 +49,4 @@ export class AdminEditExerciseComponent implements OnInit {
       editable: true
     }
   }
-
-
 }
