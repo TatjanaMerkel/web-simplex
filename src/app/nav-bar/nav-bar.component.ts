1830021d5dda26becf186075b2add96d4ae67644
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-08-14 10:15:17 +0200

    Go to previous page when clicking back button (instead of home)

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Component} from '@angular/core'
+import {Location} from '@angular/common'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor(public router: Router) {
+
+  constructor(private location: Location,
+              public router: Router) {
   }
 
-  ngOnInit(): void {
-
+  onBack() {
+    this.location.back()
   }
 }
