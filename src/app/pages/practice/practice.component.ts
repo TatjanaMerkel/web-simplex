dd0a76aca0168794595b3cd3201066503888402f
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-08-15 19:01:18 +0200

    Show list of exercises in practice page

--- src/app/pages/practice/practice.component.ts
+++ src/app/pages/practice/practice.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 
 import {HeaderService} from '../../../services/header.service'
+import {Exercise} from "../../../models/exercise";
+import {ExerciseService} from "../../../services/exercise.service";
+import {Difficulty} from "../../../models/difficulty";
+
 
 @Component({
   selector: 'app-practice',
@@ -9,10 +13,27 @@ import {HeaderService} from '../../../services/header.service'
 })
 export class PracticeComponent implements OnInit {
 
-  constructor(private headerService: HeaderService) {
+  easyExercises: Exercise[] = []
+  mediumExercises: Exercise[] = []
+  hardExercises: Exercise[] = []
+
+  constructor(private exerciseService: ExerciseService,
+              private headerService: HeaderService) {
   }
 
   ngOnInit(): void {
     this.headerService.title.next('Üben')
+
+    this.exerciseService.getExercises().subscribe(exercises => {
+      this.easyExercises = exercises
+        .filter(exercise => exercise.difficulty === Difficulty.EASY)
+
+      this.mediumExercises = exercises
+        .filter(exercise => exercise.difficulty === Difficulty.MEDIUM)
+
+      this.hardExercises = exercises
+        .filter(exercise => exercise.difficulty === Difficulty.HARD)
+    })
+
   }
 }
