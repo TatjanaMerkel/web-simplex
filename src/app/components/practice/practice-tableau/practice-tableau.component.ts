10cac374431c2461f755556f05e27bcaca0b2e53
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-08-27 16:39:53 +0200

    Remove unused practice-tableau component

--- src/app/components/practice/practice-tableau/practice-tableau.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core'
-
-@Component({
-  selector: 'app-practice-tableau',
-  templateUrl: './practice-tableau.component.html',
-  styleUrls: ['./practice-tableau.component.css']
-})
-export class PracticeTableauComponent {
-}
